67c30b5db1a6ace08ade6166724a6f4a784a866d
Disasters07 <36681040+Disasters07@users.noreply.github.com>
2018-02-21 00:22:28 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,12 +13,22 @@ bot.login("NDE1NjIxMjExNzk2NTM3MzQ0.DW4lEg.7_oo2cQhXENnEAFDvVzwrVSg_tU");
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help / !Salut / !Ta gueule");
+        message.channel.sendMessage("Liste des commandes: \n -*help / !Salut / !Herbi / !Carni");
     }
 
     if (message.content === "!Salut"){
         message.reply("Salut à toi, Humain");
         console.log("Commande !Salut effectué");
+    }
+    
+    if (message.content === "!Herbi"){
+        message.reply("!Psyta...");
+        console.log("Commande !Herbi effectué");
+    }
+    
+    if (message.content === "!Carni"){
+        message.reply("!Velo...");
+        console.log("Commande !Carni effectué");
     }
 
     if (message.content === "!Ta gueule"){
